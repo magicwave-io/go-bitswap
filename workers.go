5c13e2011d3d934dddeca09c38c44a6fab17a904
a.gorulev <a.gorulev@kernelics.com>
2024-03-28 14:12:39 +0000

    magicwave.io

---
+++ workers.go
@@ -0,0 +1,104 @@
+package bitswap
+
+import (
+	"context"
+	"math/rand"
+	"time"
+
+	bsmsg "github.com/ipfs/go-bitswap/message"
+
+	logging "github.com/ipfs/go-log"
+	process "github.com/jbenet/goprocess"
+)
+
+var TaskWorkerCount = 8
+
+func (bs *Bitswap) startWorkers(px process.Process, ctx context.Context) {
+	// Start up workers to handle requests from other nodes for the data on this node
+	for i := 0; i < TaskWorkerCount; i++ {
+		i := i
+		px.Go(func(px process.Process) {
+			bs.taskWorker(ctx, i)
+		})
+	}
+
+	// Start up a worker to manage periodically resending our wantlist out to peers
+	px.Go(func(px process.Process) {
+		bs.rebroadcastWorker(ctx)
+	})
+}
+
+func (bs *Bitswap) taskWorker(ctx context.Context, id int) {
+	idmap := logging.LoggableMap{"ID": id}
+	defer log.Debug("bitswap task worker shutting down...")
+	for {
+		log.Event(ctx, "Bitswap.TaskWorker.Loop", idmap)
+		select {
+		case nextEnvelope := <-bs.engine.Outbox():
+			select {
+			case envelope, ok := <-nextEnvelope:
+				if !ok {
+					continue
+				}
+				log.Event(ctx, "Bitswap.TaskWorker.Work", logging.LoggableF(func() map[string]interface{} {
+					return logging.LoggableMap{
+						"ID":     id,
+						"Target": envelope.Peer.Pretty(),
+						"Block":  envelope.Block.Cid().String(),
+					}
+				}))
+
+				// update the BS ledger to reflect sent message
+				// TODO: Should only track *useful* messages in ledger
+				outgoing := bsmsg.New(false)
+				outgoing.AddBlock(envelope.Block)
+				bs.engine.MessageSent(envelope.Peer, outgoing)
+
+				bs.wm.SendBlock(ctx, envelope)
+				bs.counterLk.Lock()
+				bs.counters.blocksSent++
+				bs.counters.dataSent += uint64(len(envelope.Block.RawData()))
+				bs.counterLk.Unlock()
+			case <-ctx.Done():
+				return
+			}
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+func (bs *Bitswap) rebroadcastWorker(parent context.Context) {
+	ctx, cancel := context.WithCancel(parent)
+	defer cancel()
+
+	broadcastSignal := time.NewTicker(rebroadcastDelay.Get())
+	defer broadcastSignal.Stop()
+
+	tick := time.NewTicker(10 * time.Second)
+	defer tick.Stop()
+
+	for {
+		log.Event(ctx, "Bitswap.Rebroadcast.idle")
+		select {
+		case <-tick.C:
+			n := bs.wm.wl.Len()
+			if n > 0 {
+				log.Debug(n, " keys in bitswap wantlist")
+			}
+		case <-broadcastSignal.C: // resend unfulfilled wantlist keys
+			log.Event(ctx, "Bitswap.Rebroadcast.active")
+			entries := bs.wm.wl.Entries()
+			if len(entries) == 0 {
+				continue
+			}
+
+			// TODO: come up with a better strategy for determining when to search
+			// for new providers for blocks.
+			i := rand.Intn(len(entries))
+			bs.network.FindProviders(ctx, entries[i].Cid)
+		case <-parent.Done():
+			return
+		}
+	}
+}
