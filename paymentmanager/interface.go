e65c54d9f92204234703a041721ed57ed15ac591
a.gorulev <a.gorulev@kernelics.com>
2024-12-25 00:15:06 +0000

    magicwave.io

---
+++ paymentmanager/interface.go
@@ -0,0 +1,19 @@
+package paymentmanager
+
+import (
+	"context"
+)
+
+type ClientHandler interface {
+	ProcessCommand(commandId string, commandType int32, commandBody []byte, nodeId string, sessionId string) error
+	ProcessPayment(paymentRequest string, nodeId string)
+	ValidatePayment(paymentRequest string) (uint32,error)
+	CreatePaymentInfo(amount int) (string, error)
+	ProcessResponse(commandId string, responseBody []byte, nodeId string, sessionId string)
+}
+
+type CallbackHandler interface {
+	Start()
+	Shutdown(ctx context.Context)
+}
+
