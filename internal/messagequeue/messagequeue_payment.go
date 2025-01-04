12181f52227fb4eaa64e5cdff75f4360d1e2085c
a.gorulev <a.gorulev@kernelics.com>
2025-01-04 04:15:47 +0000

    magicwave.io

---
+++ internal/messagequeue/messagequeue_payment.go
@@ -0,0 +1,30 @@
+package messagequeue
+
+import (
+	"time"
+
+	bsmsg "github.com/ipfs/go-bitswap/message"
+	"paidpiper.com/payment-gateway/paymentmanager"
+)
+
+func WithPayment(mq *MessageQueue) *MessageQueueWithPayment {
+	return &MessageQueueWithPayment{
+		MessageQueue: *mq,
+	}
+}
+
+type MessageQueueWithPayment struct {
+	MessageQueue
+}
+
+// Bitswap ++
+
+func (mq *MessageQueueWithPayment) SendPaymentDataMessage(data paymentmanager.PaymentData) {
+	mq.msg.(bsmsg.PaymentBitSwapMessage).SetPaymentData(data)
+	select {
+
+	case mq.outgoingWork <- time.Now():
+
+	default:
+	}
+}
