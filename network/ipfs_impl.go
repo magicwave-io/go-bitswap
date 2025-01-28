9fa3b06a2d2c2eef8e49213ea070100511aa89fb
a.gorulev <a.gorulev@kernelics.com>
2025-01-28 10:16:49 +0000

    magicwave.io

---
+++ network/ipfs_impl.go
@@ -0,0 +1,202 @@
+package network
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"time"
+
+	bsmsg "github.com/ipfs/go-bitswap/message"
+
+	ggio "github.com/gogo/protobuf/io"
+	cid "github.com/ipfs/go-cid"
+	providers "github.com/ipfs/go-ipfs-provider"
+	logging "github.com/ipfs/go-log"
+	host "github.com/libp2p/go-libp2p-host"
+	ifconnmgr "github.com/libp2p/go-libp2p-interface-connmgr"
+	inet "github.com/libp2p/go-libp2p-net"
+	peer "github.com/libp2p/go-libp2p-peer"
+	pstore "github.com/libp2p/go-libp2p-peerstore"
+	ma "github.com/multiformats/go-multiaddr"
+)
+
+var log = logging.Logger("bitswap_network")
+
+var sendMessageTimeout = time.Minute * 10
+
+// NewFromIpfsHost returns a BitSwapNetwork supported by underlying IPFS host
+func NewFromIpfsHost(host host.Host, p providers.Interface) BitSwapNetwork {
+	bitswapNetwork := impl{
+		host:      host,
+		providers: p,
+	}
+	host.SetStreamHandler(ProtocolBitswap, bitswapNetwork.handleNewStream)
+	host.SetStreamHandler(ProtocolBitswapOne, bitswapNetwork.handleNewStream)
+	host.SetStreamHandler(ProtocolBitswapNoVers, bitswapNetwork.handleNewStream)
+	host.Network().Notify((*netNotifiee)(&bitswapNetwork))
+	// TODO: StopNotify.
+
+	return &bitswapNetwork
+}
+
+// impl transforms the ipfs network interface, which sends and receives
+// NetMessage objects, into the bitswap network interface.
+type impl struct {
+	host      host.Host
+	providers providers.Interface
+
+	// inbound messages from the network are forwarded to the receiver
+	receiver Receiver
+}
+
+type streamMessageSender struct {
+	s inet.Stream
+}
+
+func (s *streamMessageSender) Close() error {
+	return inet.FullClose(s.s)
+}
+
+func (s *streamMessageSender) Reset() error {
+	return s.s.Reset()
+}
+
+func (s *streamMessageSender) SendMsg(ctx context.Context, msg bsmsg.BitSwapMessage) error {
+	return msgToStream(ctx, s.s, msg)
+}
+
+func msgToStream(ctx context.Context, s inet.Stream, msg bsmsg.BitSwapMessage) error {
+	deadline := time.Now().Add(sendMessageTimeout)
+	if dl, ok := ctx.Deadline(); ok {
+		deadline = dl
+	}
+
+	if err := s.SetWriteDeadline(deadline); err != nil {
+		log.Warningf("error setting deadline: %s", err)
+	}
+
+	switch s.Protocol() {
+	case ProtocolBitswap:
+		if err := msg.ToNetV1(s); err != nil {
+			log.Debugf("error: %s", err)
+			return err
+		}
+	case ProtocolBitswapOne, ProtocolBitswapNoVers:
+		if err := msg.ToNetV0(s); err != nil {
+			log.Debugf("error: %s", err)
+			return err
+		}
+	default:
+		return fmt.Errorf("unrecognized protocol on remote: %s", s.Protocol())
+	}
+
+	if err := s.SetWriteDeadline(time.Time{}); err != nil {
+		log.Warningf("error resetting deadline: %s", err)
+	}
+	return nil
+}
+
+func (bsnet *impl) NewMessageSender(ctx context.Context, p peer.ID) (MessageSender, error) {
+	s, err := bsnet.newStreamToPeer(ctx, p)
+	if err != nil {
+		return nil, err
+	}
+
+	return &streamMessageSender{s: s}, nil
+}
+
+func (bsnet *impl) newStreamToPeer(ctx context.Context, p peer.ID) (inet.Stream, error) {
+	return bsnet.host.NewStream(ctx, p, ProtocolBitswap, ProtocolBitswapOne, ProtocolBitswapNoVers)
+}
+
+func (bsnet *impl) SendMessage(
+	ctx context.Context,
+	p peer.ID,
+	outgoing bsmsg.BitSwapMessage) error {
+
+	s, err := bsnet.newStreamToPeer(ctx, p)
+	if err != nil {
+		return err
+	}
+
+	if err = msgToStream(ctx, s, outgoing); err != nil {
+		s.Reset()
+		return err
+	}
+	// TODO(https://github.com/libp2p/go-libp2p-net/issues/28): Avoid this goroutine.
+	go inet.AwaitEOF(s)
+	return s.Close()
+
+}
+
+func (bsnet *impl) SetDelegate(r Receiver) {
+	bsnet.receiver = r
+}
+
+func (bsnet *impl) ConnectTo(ctx context.Context, p peer.ID) error {
+	return bsnet.host.Connect(ctx, pstore.PeerInfo{ID: p})
+}
+
+// handleNewStream receives a new stream from the network.
+func (bsnet *impl) handleNewStream(s inet.Stream) {
+	defer s.Close()
+
+	if bsnet.receiver == nil {
+		s.Reset()
+		return
+	}
+
+	reader := ggio.NewDelimitedReader(s, inet.MessageSizeMax)
+	for {
+		received, err := bsmsg.FromPBReader(reader)
+		if err != nil {
+			if err != io.EOF {
+				s.Reset()
+				go bsnet.receiver.ReceiveError(err)
+				log.Debugf("bitswap net handleNewStream from %s error: %s", s.Conn().RemotePeer(), err)
+			}
+			return
+		}
+
+		p := s.Conn().RemotePeer()
+		ctx := context.Background()
+		log.Debugf("bitswap net handleNewStream from %s", s.Conn().RemotePeer())
+		bsnet.receiver.ReceiveMessage(ctx, p, received)
+	}
+}
+
+// FindProvidersAsync returns a channel of providers for the given key
+func (bsnet *impl) FindProvidersAsync(ctx context.Context, k *cid.Cid, max int) <-chan peer.ID {
+	return bsnet.providers.FindProvidersAsync(ctx, k, max)
+}
+
+func (bsnet *impl) FindProviders(ctx context.Context, k *cid.Cid) error {
+	return bsnet.providers.FindProviders(ctx, k)
+}
+
+func (bsnet *impl) Provide(ctx context.Context, k *cid.Cid) error {
+	return bsnet.providers.Provide(k)
+}
+
+func (bsnet *impl) ConnectionManager() ifconnmgr.ConnManager {
+	return bsnet.host.ConnManager()
+}
+
+type netNotifiee impl
+
+func (nn *netNotifiee) impl() *impl {
+	return (*impl)(nn)
+}
+
+func (nn *netNotifiee) Connected(n inet.Network, v inet.Conn) {
+	nn.impl().receiver.PeerConnected(v.RemotePeer())
+}
+
+func (nn *netNotifiee) Disconnected(n inet.Network, v inet.Conn) {
+	nn.impl().receiver.PeerDisconnected(v.RemotePeer())
+}
+
+func (nn *netNotifiee) OpenedStream(n inet.Network, v inet.Stream) {}
+func (nn *netNotifiee) ClosedStream(n inet.Network, v inet.Stream) {}
+func (nn *netNotifiee) Listen(n inet.Network, a ma.Multiaddr)      {}
+func (nn *netNotifiee) ListenClose(n inet.Network, a ma.Multiaddr) {}
